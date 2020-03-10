05f5113e6a65b62aad62bd0015fc33ac19030982
Iury F. A. Ferreira <iury.franklinferreira@gmail.com>
2020-03-10 15:34:23 -0300

    all queue's functionalities implemented.

--- data_structures/queue.py
+++ data_structures/queue.py
@@ -1,79 +1,50 @@
-class Fila:
+from utils.data_structures_visualization import DataStructuresVisualization
 
-    def __init__(self, tamanho):
+class Queue:
 
-        self.fila = [None] * tamanho
-        self.tamanho = tamanho
-        self.inicio = 0
-        self.fim = 0
+    def __init__(self, size):
+
+        self.queue = [None] * size
+        self.size = size
+        self.start = 0
+        self.end = 0
 
     def __repr__(self):
 
-        queue = ""
-        def get_formated_queue(queue):
-            for i in range(self.tamanho):
-                if i != self.inicio:
-                    queue += " [  %s  ] " % self.fila[i]
-                else:
-                    queue +=" [→ %s ←] " % self.fila[i]
+        return "Queue: {0}".format(DataStructuresVisualization.queue(self))
 
-            return queue
-        
-        queue = get_formated_queue(queue)
+    def is_full(self):
 
-        return "Queue: {0}".format(queue)
-
-    def indice_seguinte(self, ponteiro):
-
-        if ponteiro == "Inicio":
-            if self.inicio == self.tamanho:
-                return 0
-            else:
-                return self.inicio
-        elif ponteiro == "Fim":
-            if self.fim == self.tamanho:
-                return 0
-            else:
-                return self.fim
-        else:
-            print("Ponteiro escrito errado.")
-
-    def fila_cheia(self):
-
-        if (self.fim == self.tamanho and self.inicio == 0) or self.fila[self.indice_seguinte("Fim")] is not None:
+        next_index = 0 if self.end == self.size else self.end
+        if self.queue[next_index] is not None:
             return True
+        return False 
+
+    def is_empty(self):
+
+        next_index = 0 if self.start == self.size else self.start
+        if self.queue[next_index] is not None:
+            return True
+        return False 
+
+    def enqueue(self, value):
+
+        if self.is_full():
+            return "The queue is full."
         else:
-            return False
+            if self.end == self.size:
+                self.end = 0
+            self.queue[self.end] = value
+            self.end = self.end + 1
 
-    def fila_vazia(self):
+    def dequeue(self):
 
-        return True if self.fila[self.indice_seguinte("Inicio")] is None else False
-
-    def enfileirar(self, valor):
-
-        if self.fila_cheia():
-            return "Fila Cheia"
+        if self.is_empty():
+            return "The queue is empty."
         else:
-            if self.fim == self.tamanho:
-                self.fim = 0
-            self.fila[self.fim] = valor
-            self.fim = self.fim + 1
-
-    def desenfileirar(self):
-
-        if self.fila_vazia():
-            return "Fila Vazia"
-        else:
-            if self.inicio == self.tamanho:
-                self.inicio = 0
-            valor = self.fila[self.inicio]
-            self.fila[self.inicio] = None
-            self.inicio = self.inicio + 1
-            return valor
-
-if __name__ == "__main__":
-    
-    f = Fila(3)
-    f.enfileirar(4)
-    print(f)
-
+            if self.start == self.size:
+                self.start = 0
+            value = self.queue[self.start]
+            self.queue[self.start] = None
+            self.start = self.start + 1
+            return value
