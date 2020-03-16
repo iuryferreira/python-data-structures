9ce281a54511b5ca223f29afdfbc4980cfe60f3a
Iury F. A. Ferreira <iury.franklinferreira@gmail.com>
2020-03-16 17:02:16 -0300

    docfix - Correction in class documentation

--- data_structures/chained_hashtable.py
+++ data_structures/chained_hashtable.py
@@ -1,12 +1,31 @@
 from data_structures.linked_list import LinkedList, Node
 from data_structures.hashtable import HashTable
 
+
 class ChainedHashTable(HashTable):
+
+    """
+        This class implements the Abstract Data Type (TAD'S) Hash Table, with it's main operations, using chaining collision treatment. 
+
+        Methods
+        -------
+        hash_func(value)
+            returns the position that the element is to be stored.
+
+        search(value)
+            Searches for a value.
+
+        insert()
+            Insert a value.
+
+        remove()
+            Removes a value.
+    """
 
     def __init__(self, size):
 
         super().__init__(size)
-        self.table = [LinkedList()] * size
+        self.table = [LinkedList() for posicao in range(size)]
 
     def insert(self, value):
 
@@ -24,12 +43,20 @@ class ChainedHashTable(HashTable):
 
 class ChainedHashDivisionTable(ChainedHashTable):
 
+    """
+        This class inherits from ChainedHashTable, and implements the hash function by division.
+    """
+
     def __init__(self, size):
 
         super().__init__(size)
 
 
 class ChainedHashMultiplicationTable(ChainedHashTable):
+
+    """
+        This class inherits from ChainedHashTable, and implements the hash function by multiplication.
+    """
 
     def __init__(self, size):
 
--- data_structures/hashtable.py
+++ data_structures/hashtable.py
@@ -1,16 +1,37 @@
-
-
+from data_structures.utils.data_structures_visualization import DataStructuresVisualization
 
 class HashTable:
+
+    """
+        This class implements the Abstract Data Type (TAD'S) Hash Table, with it's main operations.
+
+        Methods
+        -------
+        hash_func(value)
+            returns the position that the element is to be stored.
+
+        search(value)
+            Searches for a value.
+
+        insert()
+            Insert a value.
+
+        remove()
+            Removes a value.
+        """
 
     def __init__(self, size):
 
         self.size = size
         self.table = [None] * size
 
+    def __repr__(self):
+
+        return "\nHash Table:\n{0}".format(DataStructuresVisualization.hash_table(self.table))
+
     def hash_func(self, value):
 
-        return value % (self.size - 1)
+        return value % self.size
 
     def insert(self, value):
 
